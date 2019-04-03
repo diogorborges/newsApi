e429c08de2be642bc42237099fc4c63abdab151c
Diogo Borges <diogoraimondiborges@gmail.com>
2019-04-02 23:42:30 -0300

    Create base bottom navigation with initial empty fragments

---
+++ app/src/main/java/com/x0/newsapi/SearchFragment.kt
@@ -0,0 +1,12 @@
+package com.x0.newsapi
+
+import androidx.fragment.app.Fragment
+
+class SearchFragment : Fragment() {
+
+    companion object {
+        const val TITLE = "Search"
+        private const val TAG = "SearchFragment"
+    }
+
+}
