bebc0080b87a5929e1c887740221ac782db64fd3
Diogo Borges <diogodrb@hotmail.com>
2019-04-04 23:58:09 -0300

    Create datasource exceptions to handle interfaces

---
+++ app/src/main/java/com/x0/newsapi/data/Exceptions.kt
@@ -0,0 +1,7 @@
+package com.x0.newsapi.data
+
+open class DataSourceException(message: String? = null) : Exception(message)
+
+class RemoteDataNotFoundException : DataSourceException("Data not found in remote data source")
+
+class LocalDataNotFoundException : DataSourceException("Data not found in local data source")
