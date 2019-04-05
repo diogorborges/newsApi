3425876b0b408591530797720b12ce2bed3fb818
Diogo Borges <diogoraimondiborges@gmail.com>
2019-04-05 19:07:15 -0300

    Setup newapirepository and add some logs

--- app/src/main/java/com/x0/newsapi/data/NewsApiRepository.kt
+++ app/src/main/java/com/x0/newsapi/data/NewsApiRepository.kt
@@ -1,6 +1,7 @@
 package com.x0.newsapi.data
 
 import android.annotation.SuppressLint
+import android.util.Log
 import com.x0.newsapi.common.ListUtils
 import com.x0.newsapi.data.local.NewsApiLocalDataSource
 import com.x0.newsapi.data.model.news.NewsResponse
@@ -9,12 +10,17 @@ import com.x0.newsapi.data.remote.NewsApiRemoteDataSource
 import io.reactivex.Completable
 import io.reactivex.Single
 import io.reactivex.schedulers.Schedulers
+import timber.log.Timber
 import javax.inject.Inject
 
 class NewsApiRepository @Inject constructor(
-    val newsApiRemoteDataSource: NewsApiRemoteDataSource,
-    val newsApiLocalDataSource: NewsApiLocalDataSource
+    private val newsApiRemoteDataSource: NewsApiRemoteDataSource,
+    private val newsApiLocalDataSource: NewsApiLocalDataSource
 ) : NewsApiDataSource {
+
+    companion object {
+        const val TAG = "NewsApiRepository"
+    }
 
     override fun getFavoriteSources(isFavorite: Boolean): Single<ArrayList<Source>> =
         newsApiLocalDataSource.getFavoriteSources(isFavorite)
@@ -26,28 +32,35 @@ class NewsApiRepository @Inject constructor(
         newsApiLocalDataSource.getSources()
             .flatMap {
                 if (it.isNotEmpty()) {
+                    Log.i(TAG, "Dispatching ${it.size} sources from DB...")
                     return@flatMap Single.just(it)
                 } else {
-                    return@flatMap deleteAndFetchSources()
+                    return@flatMap fetchAndPersistSources()
                 }
             }
 
-    private fun deleteAndFetchSources(): Single<ArrayList<Source>> =
-        deleteSources().andThen(fetchAndPersistSources())
-
     private fun fetchAndPersistSources(): Single<ArrayList<Source>> =
-        newsApiRemoteDataSource.getSources().doOnSuccess(this::persistSources)
+        newsApiRemoteDataSource.getSources()
+            .doOnSuccess {
+                Log.i(TAG, "Dispatching ${it.size} sources from API...")
+                persistSources(it)
+            }
 
     @SuppressLint("CheckResult")
     private fun persistSources(sources: ArrayList<Source>) {
         insertSources(*ListUtils.toArray(Source::class.java, sources))
             .subscribeOn(Schedulers.io())
+            .observeOn(Schedulers.io())
+            .subscribe(
+                { Log.i(TAG, "Success persisting sources...") },
+                { Log.e(TAG, "Failure persisting sources...") })
     }
 
     override fun insertSources(vararg sources: Source): Completable =
         newsApiLocalDataSource.insertSources(*sources)
 
     override fun deleteSources(): Completable = newsApiLocalDataSource.deleteSources()
+        .doOnError { Log.e(TAG, "Failure deleting sources...") }
 
     override fun getSourcesBySubject(subject: String): Single<NewsResponse> =
         newsApiRemoteDataSource.getSourcesBySubject(subject)
